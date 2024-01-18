3cc37beeeb210b1089500ff2e169b9d393308025
zhangyix19 <zhangyix19@mails.tsinghua.edu.cn>
2024-01-18 13:36:12 +0800

    fix

--- builddocker.sh
+++ builddocker.sh
@@ -1,11 +1,11 @@
 docker build -t ewfd-data-base -f Docker/Dockerfile.base .
 docker build -t ewfd-data-tor -f Docker/Dockerfile.tor .
-docker build -t ewfd-data-data -f Docker/Dockerfile.data .
+docker build -t ewfd-data-tbb -f Docker/Dockerfile.tbb .
 docker tag ewfd-data-base:latest zhangyix19/ewfd-data:base
 docker tag ewfd-data-tor:latest zhangyix19/ewfd-data:tor
-docker tag ewfd-data-data:latest zhangyix19/ewfd-data:data
-docker tag ewfd-data-data:latest zhangyix19/ewfd-data:dev
+docker tag ewfd-data-tbb:latest zhangyix19/ewfd-data:tbb
+docker tag ewfd-data-tbb:latest zhangyix19/ewfd-data:dev
 docker push zhangyix19/ewfd-data:base
 docker push zhangyix19/ewfd-data:tor
-docker push zhangyix19/ewfd-data:data
+docker push zhangyix19/ewfd-data:tbb
 docker push zhangyix19/ewfd-data:dev
