4762bdb44847f3d8a9e2dbefa2b2aaa841f68d47
zhangyix19 <zhangyix19@mails.tsinghua.edu.cn>
2024-01-18 12:02:42 +0800

    add docker push

--- builddocker.sh
+++ builddocker.sh
@@ -1,3 +1,11 @@
 docker build -t ewfd-data-base -f Docker/Dockerfile.base .
 docker build -t ewfd-data-tor -f Docker/Dockerfile.tor .
 docker build -t ewfd-data-data -f Docker/Dockerfile.data .
+docker tag ewfd-data-base:latest ewfd-data:base
+docker tag ewfd-data-tor:latest ewfd-data:tor
+docker tag ewfd-data-data:latest ewfd-data:data
+docker tag ewfd-data-data:latest ewfd-data:dev
+docker push zhangyix19/ewfd-data:base
+docker push zhangyix19/ewfd-data:tor
+docker push zhangyix19/ewfd-data:data
+docker push zhangyix19/ewfd-data:dev
