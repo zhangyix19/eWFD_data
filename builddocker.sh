099e7d3769a3ad2171df6a1d86a63c56f0027148
zhangyix19 <zhangyix19@mails.tsinghua.edu.cn>
2024-01-18 19:55:23 +0800

    update dockerfile

--- builddocker.sh
+++ builddocker.sh
@@ -1,14 +1,10 @@
-docker build -t ewfd-data-base -f Docker/Dockerfile.base .
-docker build -t ewfd-data-tor -f Docker/Dockerfile.tor .
-docker build -t ewfd-data-tbb -f Docker/Dockerfile.tbb .
-docker tag ewfd-data-base:latest zhangyix19/ewfd-data:base
-docker tag ewfd-data-tor:latest zhangyix19/ewfd-data:tor
-docker tag ewfd-data-tbb:latest zhangyix19/ewfd-data:tbb
-docker tag ewfd-data-tbb:latest zhangyix19/ewfd-data:dev
+docker build -t zhangyix19/ewfd-data:base -f Docker/Dockerfile.base .
+docker build -t zhangyix19/ewfd-data:tor -f Docker/Dockerfile.tor .
+docker build -t zhangyix19/ewfd-data:tbb -f Docker/Dockerfile.tbb .
+
 docker push zhangyix19/ewfd-data:base
 docker push zhangyix19/ewfd-data:tor
 docker push zhangyix19/ewfd-data:tbb
-docker push zhangyix19/ewfd-data:dev
 
 docker build -t zhangyix19/ewfd-data:data-undefend -f Docker/data/Dockerfile.undefended .
 docker push zhangyix19/ewfd-data:data-undefend
